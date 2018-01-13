550c1b107cb9b49cf5c3bb19972d950ce0398062
Baljon <a.baljon@student.uw.edu.pl>
2018-01-13 16:26:07 +0100

    change coordinates and add test values

--- ptaszki.py
+++ ptaszki.py
@@ -8,10 +8,15 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig = plt.figure()
-ax = plt.axes(xlim=(0, 640), ylim=(0, 480))
+ax = plt.axes(xlim=(0, 800), ylim=(0, 600))
 
-pts = ax.plot([x], [y], markersize=10, c='black', marker='o', ls='None')
-beak = ax.plot([x+r], [y+r], markersize=4, c='red', marker='o', ls='None')
+#testowe wspolrzedne
+x=100
+y=400
+r=10
+
+pts = ax.plot([x], [y], markersize=10, c='skyblue', marker='o', ls='None')
+beak = ax.plot([x+r], [y+r], markersize=4, c='brown', marker='o', ls='None')
 
 plt.show()
 
